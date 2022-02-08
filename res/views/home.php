9e18daef2c3e5ffbc45214f4ffeb03dad95a0929
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-08 22:04:22 +0100

    :art: update he homepage render

--- res/views/home.php
+++ res/views/home.php
@@ -13,7 +13,13 @@
     <body>
         <main>
             <h1><?= text('app_name') ?></h1>
-            <p><?= ucfirst(text('welcome_to', text('app_name'))) ?></p>
+            <p><?= ucfirst(text('Welcome_to', text('app_name'))) ?></p>
+            <form action="#" method="GET">
+                <p>
+                    <input class="field" type="email" name="email" placeholder="<?= text('Enter_email') ?>"/>
+                    <button class="button" name="action" type="submit" value="continue"><?= text('Continue') ?></button>
+                </p>
+            </form>
         </main>
     </body>
 </html>
